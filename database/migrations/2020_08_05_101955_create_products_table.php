4bd062a7bf64280c2b12efc82b8ef26f9e4bb44e
seanyboyswag <2150218@slu.edu.ph>
2020-08-06 14:27:22 +0800

    Added database migration for products table

---
+++ database/migrations/2020_08_05_101955_create_products_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description', 250)
+                ->nullable();
+            $table->string('image_url')
+                ->index();
+            $table->boolean('is_on_sale')
+                ->default(TRUE);
+            $table->unsignedDecimal('shipping_cost', 8, 4)
+                ->default(0);
+            $table->unsignedDecimal('handling_cost', 8, 4)
+                ->default(0);
+            $table->unsignedDecimal('product_cost', 8, 4)
+                ->default(0);
+            $table->unsignedDecimal('sale_product_cost', 8, 4)
+                ->default(0);
+            $table->unsignedDecimal('final_cost', 8, 4)
+                ->default(0);
+            $table->json('attributes')
+                ->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
