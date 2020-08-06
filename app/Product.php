baeb7d36fee4cb90858daab022a697de72bfac74
seanyboyswag <2150218@slu.edu.ph>
2020-08-06 14:41:50 +0800

    Product Model

---
+++ app/Product.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'products';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'stock',
+        'image_url',
+        'is_on_sale',
+        'shipping_cost',
+        'handling_cost',
+        'product_cost',
+        'sale_product_cost',
+        'final_cost',
+        'attributes',
+    ];
+}
