f982b59a8dbaf87d0963236bab4a3303170d0183
Christopher Espiritu <christopheredrian@gmail.com>
2020-08-09 19:20:38 -0400

    Update AwsController.php

--- app/Http/Controllers/AwsController.php
+++ app/Http/Controllers/AwsController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\AwsSnsLog;
 use Illuminate\Http\Request;
 
 class AwsController extends Controller
 {
     public function sns(Request $request){
-        var_dump($request->header());
-        dd($request->post());
+        $sns = new AwsSnsLog();
+        $sns->identifier = "sns";
+        $sns->headers = $request->header();
+        $sns->raw_json = $request->all();
+        $sns->ip = $request->ip();
+        $sns->saveOrFail();
     }
 }
