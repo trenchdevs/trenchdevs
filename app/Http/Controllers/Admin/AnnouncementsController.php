ce74008d0229e579abb53dfbb0d3a3d43d1d69dc
Christopher Espiritu <christopheredrian@gmail.com>
2020-08-27 20:35:25 -0400

    Update AnnouncementsController.php

--- app/Http/Controllers/Admin/AnnouncementsController.php
+++ app/Http/Controllers/Admin/AnnouncementsController.php
@@ -73,7 +73,7 @@ class AnnouncementsController extends Controller
             foreach ($emails as $email) {
 
                 $viewData = [
-                    'name' => $user->name(),
+                    'name' => null,
                     'email_body' => $message,
                 ];
 
