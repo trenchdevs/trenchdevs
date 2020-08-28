e95ba11c57f1a486564daa841db63a474ba7d2e4
Christopher Espiritu <christopheredrian@gmail.com>
2020-08-28 19:29:44 -0400

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('send:emails')->everyFiveMinutes();
-        $schedule->command('inspire')->everyMinute()->appendOutputTo('~/inspire.txt');
+        // $schedule->command('inspire')->everyMinute()->appendOutputTo('~/inspire.txt');
     }
 
     /**
