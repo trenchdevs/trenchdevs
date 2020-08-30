e43bf19557d0a6e4a475e8ff1fa2e6759daeb49e
Christopher Espiritu <christopheredrian@gmail.com>
2020-08-30 13:22:02 -0400

    Update SiteAccessMiddleware.php

--- app/Http/Middleware/SiteAccessMiddleware.php
+++ app/Http/Middleware/SiteAccessMiddleware.php
@@ -19,18 +19,49 @@ class SiteAccessMiddleware
 
         $user = $request->user();
 
+        $miscArr = [
+            'request_encoded' => base64_encode(json_encode($request->all())),
+            'headers' => $request->headers,
+        ];
+
+        $requestData = $request->all();
+        $userAgent = $request->header('User-Agent');
+
+        if ($userAgent === 'ELB-HealthChecker/2.0') {
+            /**
+             * Do not log elb health checks
+             */
+            return $next($request);
+        }
+
+        if (!empty($requestData)) {
+            $miscArr['request_encoded'] = $requestData;
+        }
+
+        $headers = collect($request->header())->transform(function ($item) {
+            return $item[0];
+        });
+
+        if (!empty($headers)) {
+            $miscArr['headers'] = $headers;
+        }
+
+
         $siteAccess = new SiteAccessLog();
-        $siteAccess->fill([
+
+        $data = [
             'user_id' => $user->id ?? null,
             'url' => $request->fullUrl(),
             'ip' => $request->ip(),
-            'user_agent' => $request->header('User-Agent'),
+            'user_agent' => $userAgent,
             'referer' => $request->header('referer'),
-            'misc_json' => json_encode([
-                'request_encoded' => base64_encode(json_encode($request->all())),
-                'headers' => $request->headers,
-            ]),
-        ]);
+        ];
+
+        if (!empty($miscArr)) {
+            $data['misc_json'] = json_encode($miscArr);
+        }
+
+        $siteAccess->fill($data);
         $siteAccess->save();
 
         return $next($request);
