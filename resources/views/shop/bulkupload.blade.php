36b8c4445984814290e322bfa35c4fc50e52361c
seanyboyswag <2150218@slu.edu.ph>
2020-09-14 16:44:08 +0800

    Removed dead code in Products bulk upload view

--- resources/views/shop/bulkupload.blade.php
+++ resources/views/shop/bulkupload.blade.php
@@ -11,20 +11,6 @@
 
         <div class="card-body p-3">
 
-            @if(session()->has('errors'))
-                <div class="alert alert-danger p-2 pb-0">
-                    {{ session()->get('errors')  }}
-                </div>
-            @elseif(session()->has('success'))
-                <div class="alert alert-success p-2 pb-0">
-                    {{ session()->get('success') }}
-                </div>
-            @elseif(session()->has('success w/ errors'))
-                <div class="alert alert-warning p-2 pb-0">
-                    {{ session()->get('success w/ errors') }}
-                </div>
-            @endif
-
             <div class="content d-flex justify-content-center p-4">
                 <form method="POST" action="{{route('shop.bulk-upload')}}" enctype="multipart/form-data">
                     @csrf
