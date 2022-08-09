d8888ca7d4c017abb0a6023344dbaba3ca9de90e
Christopher Espiritu <christopheredrian@gmail.com>
2022-08-08 22:18:48 -0400

    fe: 20220808 build

---
+++ backend/public/build/assets/Dashboard.1da56717.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app.d4ee405e.js";import{T as a}from"./TrenchDevsAdminLayout.8a93d73e.js";function n(o){const r={demo:a}.demo;return t(r,{auth:o.auth,errors:o.errors})}export{n as default};
---
+++ backend/public/build/assets/Dashboard.9a51dba9.js
@@ -0,0 +1,1 @@
+import{j as a}from"./app.d4ee405e.js";import{T as o}from"./TrenchDevsAdminLayout.8a93d73e.js";function h(r){return a(o,{auth:r.auth,errors:r.errors,children:a("h2",{children:"Dashboard"})})}export{h as default};
