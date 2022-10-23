aac56e6719f323c97609ce640ceb23c5c4710aa2
Christopher Espiritu <christopheredrian@gmail.com>
2022-10-23 08:34:24 -0400

    sec enhancements

--- backend/config/session.php
+++ backend/config/session.php
@@ -128,7 +128,7 @@ return [
     |
     */
 
-    'cookie' => env(
+    'cookie' => '__Secure' . env(
         'SESSION_COOKIE',
         Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
     ),
@@ -175,7 +175,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE'),
+    'secure' => env('SESSION_SECURE_COOKIE', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -203,6 +203,6 @@ return [
     |
     */
 
-    'same_site' => 'lax',
+    'same_site' => 'strict',
 
 ];
