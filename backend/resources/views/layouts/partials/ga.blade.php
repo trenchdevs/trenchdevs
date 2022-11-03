b10a0805104b6e2532732fb67e299b79ffbf01e4
Christopher Espiritu <christopheredrian@gmail.com>
2022-11-03 14:55:50 -0400

    GA4

--- backend/resources/views/layouts/partials/ga.blade.php
+++ backend/resources/views/layouts/partials/ga.blade.php
@@ -1,15 +1,25 @@
 @if(env('APP_ENV') === 'production')
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-176783752-1"></script>
+{{--    <!-- Global site tag (gtag.js) - Google Analytics -->--}}
+{{--    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-176783752-1"></script>--}}
+{{--    <script>--}}
+{{--        window.dataLayer = window.dataLayer || [];--}}
+
+{{--        function gtag() {--}}
+{{--            dataLayer.push(arguments);--}}
+{{--        }--}}
+
+{{--        gtag('js', new Date());--}}
+
+{{--        gtag('config', 'UA-176783752-1');--}}
+{{--    </script>--}}
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-2GG77DGKJY"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-
+        function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
-        gtag('config', 'UA-176783752-1');
+        gtag('config', 'G-2GG77DGKJY');
     </script>
 @endif
